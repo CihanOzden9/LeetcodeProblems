3ebb72f0106d7dd4398eee1800409bc2fa29eb25
Cihan Ozden <cihanozdencou@gmail.com>
2025-09-29 20:24:55 +0300

    Reversed Linked list solving

---
+++ linked_list/reversed_linkedlist.py
@@ -0,0 +1,9 @@
+def func(head):
+    ref = None
+    current = head
+    while current:
+        next_node = current.next
+        current.next = ref
+        ref = current
+        current = next_node
+    return ref
