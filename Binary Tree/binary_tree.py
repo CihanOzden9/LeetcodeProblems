d750f29b0911b1aa9d743f6ffb889c194efc70e6
Cihan Ozden <cihanozdencou@gmail.com>
2025-12-17 11:26:03 +0300

    Binary tree added

--- Binary Tree/binary_tree.py
+++ Binary Tree/binary_tree.py
@@ -1,7 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  3 10:36:08 2025
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
 
-@author: Cio
-"""
+class BinaryTree:
+    def __init__(self):
+        self.root = None
 
+    def insert(self, value):
+        new_node = Node(value)
+
+        if self.root is None:
+            self.root = new_node
+            return True
+        
+        temp_node = self.root
+
+        while True:
+            if new_node.value == temp_node.value:
+                return False
+            
+            if new_node.value < temp_node.value:
+                if temp_node.left is None:
+                    temp_node.left = new_node
+                    return True
+                temp_node = temp_node.left
+            
+            else:
+                if temp_node.right is None:
+                    temp_node.right = temp_node
+                    return True
+                temp_node = temp_node.right
